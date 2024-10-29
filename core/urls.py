afa0f3364b1b307ea3bf81908ff9e2d99f741a68
gabreilmjrs <gabriel56142746@edu.pa.senac.br>
2024-10-29 17:28:48 -0300

    criaçao da rota e views

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from visualdef import views
 
 urlpatterns = [
+
+    path('', views.descricao, name="Descricao.html"),
     path('admin/', admin.site.urls),
 ]
--- visualdef/views.py
+++ visualdef/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def descricao(request):
+        return render(request, "Descricao.html")
