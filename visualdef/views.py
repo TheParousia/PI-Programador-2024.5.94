27632c11f4c01aa64a45b0088882c31efe664cca
10526816 <maria10526816@edu.pa.senac.br>
2024-10-29 17:04:13 -0300

    criação da view comando ,para receber dados do front end.

--- visualdef/views.py
+++ visualdef/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from.models import visualdef,formularioenvio
+# Create your views here.
+def formularioenvio(request):
+    return render(request,"visualdef.html")
+
+def comando(request):
+    comando=request.GET.get("comando")
+    
+
+    contexto={
+        "comando":comando,
+        
+    }
+
+    return render(request,"visualdef.HTML", contexto)
+    
+   
