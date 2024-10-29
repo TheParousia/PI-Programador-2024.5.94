b2a27d15ae70c18d16350ebf1a41e9e4a5d48806
vitor-nc <vitor56103456@edu.pa.senac.br>
2024-10-29 17:47:26 -0300

    modificaçao do formulario e da views

--- visualdef/views.py
+++ visualdef/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+import google.generativeai as genai
+import os
 
 # Create your views here.
 def descricao(request):
-        return render(request, "Descricao.html")
+
+
+        remetente = request.GET.get("remetente")
+        print(remetente)
+
+        contexto = {}
+
+        if remetente is not None:
+                
+                genai.configure(api_key="AIzaSyCrJHeuVlhGitTdgmnlDY_i7ETfiRMFTC0")
+
+                model = genai.GenerativeModel("gemini-1.5-flash")
+                response = model.generate_content(remetente)
+                print(response.text),
+
+                contexto = {
+                "remetente": remetente,
+                "resposta": response.text
+                }
+
+        return render(request, "formulario.html", contexto)
