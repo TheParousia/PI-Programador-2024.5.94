c42818fb5e2be00d8c46f08ffadd54dd730bf2d8
10526816 <maria10526816@edu.pa.senac.br>
2024-10-29 16:47:32 -0300

    criação da views

--- visualdef/views.py
+++ visualdef/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from.models import visualdef,formularioenvio
+
+def formularioenvio(request):
+    return render(request,"formularioenvio")
+
+def formularioenvio(request):
+    comando=request.GET.get("comando")
+   
+    if comando!=None:
+        #criação do objeto usando a classe cartão
+        visualdef=visualdef()
+        #Uso dos dados vindo do front-end
+        #para preencher o objeto cartão
+        formularioenvio.comando=comando
+        
+        visualdef.visualdef()
+
+    contexto={
+        "comando":comando,
+        
+    }
+
+
+    return render(request,"visualdef.html",contexto)
+    
 
 # Create your views here.
