7bca76e68276740b5edda7f7202262633a10d849
hugodele <hugo50671706@edu.pa.senac.br>
2024-11-04 14:17:10 -0300

    adcDeIA

--- visualdef/views.py
+++ visualdef/views.py
@@ -7,27 +7,38 @@ import os
 def formulario(request):
 
     contexto = {}
+
     if request.method == "POST":
-        remetente = request.GET.get("remetente")
+        #Recebe os dados do formulário
+        remetente = request.POST.get("remetente")
         print(remetente)
 
         contexto = {}
 
-        if remetente is not None:
+        #Código para salvar a imagem
+        if "imagem" in request.FILES:
+            cartao = Cartao()
+
+            cartao.imagem = request.FILES["imagem"]
+            cartao.remetente = remetente
+
+            cartao.save()
+            
+            #Código para enviar a imagem para a IA
             GOOGLE_API_KEY = "AIzaSyCrJHeuVlhGitTdgmnlDY_i7ETfiRMFTC0"
 
             genai.configure(api_key=GOOGLE_API_KEY)
 
             model = genai.GenerativeModel("gemini-1.5-flash")
             imagemCarregada = PIL.Image.open(cartao.imagem)
-            response = model.generate_content(remetente)
+            response = model.generate_content(["Gere um texto descrevendo a imagem para uma pessoa cega: \n", imagemCarregada])
             print(response.text),
 
             mensagem = response.text
 
         contexto = {
             "remetente": remetente,
-            "resposta": response.text
+            "resposta": response.text,
         }
 
     return render(request, "formulario.html", contexto)
