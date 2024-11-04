316442400ec0dec891393e1ab5888dcfa37fb761
hugodele <hugo50671706@edu.pa.senac.br>
2024-11-04 14:27:54 -0300

    modificacaoDeViwsEurls

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from visualdef import views
+from cartao import views
 
 urlpatterns = [
 
     path('', views.descricao, name="Descricao.html"),
     path('admin/', admin.site.urls),
-]
+]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
