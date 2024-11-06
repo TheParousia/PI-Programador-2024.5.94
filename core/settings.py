6b0f46df3d6f7407c2df711d3254e41433cc19b6
hugodele <hugo50671706@edu.pa.senac.br>
2024-11-06 16:29:55 -0300

    adcDeDuasLinhasEmSettings

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
