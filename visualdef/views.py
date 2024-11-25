f4ec6df04f0109734d708c83e25c696fb4f1ca06
Gustxz23 <firmino54381526@edu.pa.senac.br>
2024-11-25 17:58:26 -0300

    alteracao-frontend

--- visualdef/views.py
+++ visualdef/views.py
@@ -54,24 +54,6 @@ def formulario(request):
     return render(request, "formulario.html")
 
 
-def ler_img(request):
-
-    context = {}
-
-    # Recebe os dados da imagem.
-    if request.method == "POST":
-
-        # Deserializando a imagem
-        img = request.POST.get('imagem')
-        imgSerializedSplited = base64.b64decode(img.split(',')[1])
-
-        # Armazenando a imagem.
-        request.FILES["imagem"] = ContentFile(
-            imgSerializedSplited, name='imgSerializedSplited.jpg')
-
-        cartao = Cartao()
-
-
 def formularioenvio(request):
     return render(request, "formularioenvio")
 
@@ -123,7 +105,42 @@ def upload_imagem(request):
 
 
 def webcam(request):
-    return render(request, "webcam.html")
+
+    context = {}
+
+    if request.method == 'POST':
+        img = request.POST.get('imagem')
+
+        imgSerializedSplit = base64.b64decode(
+            img.split(',')[1]
+        )
+
+        request.FILES['imagem'] = ContentFile(
+            imgSerializedSplit, name='imgSerializedSplit.jpg'
+        )
+
+        cartao = Cartao()
+
+        cartao.imagem = request.FILES['imagem']
+
+        cartao.save()
+
+        genai.configure(api_key="AIzaSyCrJHeuVlhGitTdgmnlDY_i7ETfiRMFTC0")
+
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        imagemCarreda = PIL.Image.open(cartao.imagem)
+        response = model.generate_content(
+            ["Gere um texto descrevendo a imagem, para uma pessoa cega: ", imagemCarreda])
+        print(response.text),
+
+        mensagem = response.text
+
+        context = {
+            "descricao": mensagem,
+            "resposta": cartao,
+        }
+
+    return render(request, "webcam.html", context)
 
 
 def visualdef(request):
